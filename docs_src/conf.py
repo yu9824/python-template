02069257017ac483206b757479a0dac1cf345214
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-05-06 12:20:14 +0900

    Update conf.py

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -26,7 +26,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "_notes"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # markdown
 source_suffix = [".rst", ".md"]
