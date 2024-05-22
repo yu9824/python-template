6ef9625ddf1208f1b5ad51cfba48971968d1f3b7
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-05-22 22:55:04 +0900

    Update docs_src/conf.py

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-from template import __author__, __version__
+from python_template import __author__, __version__
 
-project = "template"
+project = "python-template"
 copyright = "2024, yu9824"
 author = __author__
 release = __version__
