92f41bed1020df366c627964a912e30430c6a919
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-06-05 07:46:26 +0900

    Update setup.cfg (change package name)

--- src/template/__init__.py
+++
@@ -1,9 +0,0 @@
-"""My own python package's template."""
-
-__version__ = "0.0.1"
-__license__ = "MIT"
-__author__ = "yu9824"
-__copyright__ = "Copyright © 2024 yu9824"
-__url__ = "https://github.com/yu9824/template/"
-
-# __all__ = ()
