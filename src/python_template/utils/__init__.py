ab96b57e3aa79b9bd1eb0d4c17df26dee05a2658
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-04-19 11:04:50 +0900

    docs: Update docstrings

--- src/python_template/utils/__init__.py
+++ src/python_template/utils/__init__.py
@@ -5,11 +5,6 @@ making it easy to incorporate them into different parts of the project. These ut
 are intended to simplify common tasks, such as checking if a package is installed,
 iterating with a dummy progress bar, and verifying function arguments.
 
-Modules
--------
-- dummy_tqdm: A dummy class that mimics the behavior of 'tqdm' for testing or placeholder purposes.
-- is_argument: A function that checks if a given argument name exists in a callable's signature.
-- is_installed: A function that checks if a specified package is installed.
 """
 
 from ._utils import dummy_tqdm, is_argument, is_installed
