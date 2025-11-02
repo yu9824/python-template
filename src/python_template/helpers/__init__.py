d0910a53d2184a29134570ebdd78ae526f0a4660
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-11-02 22:28:07 +0900

    refactor: Rename utils to helpers

--- src/python_template/helpers/__init__.py
+++ src/python_template/helpers/__init__.py
@@ -7,8 +7,8 @@ iterating with a dummy progress bar, and verifying function arguments.
 
 """
 
-from ._utils import dummy_tqdm, is_argument, is_installed
+from ._helpers import dummy_tqdm, is_argument, is_installed
 
-# _utils.pyだと、_が入っているのでドキュメント化されない。
+# _helpers.pyだと、_が入っているのでドキュメント化されない。
 # ドキュメント化したい場合は、モジュールメソッドとして登録するため、__all__に入れる。
 __all__ = ("dummy_tqdm", "is_argument", "is_installed")
